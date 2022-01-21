1ae61639680e95f50f98842099e8db73ff1c2eb7
LucassNogueira <simplylucasjason@gmail.com>
2022-01-21 12:22:47 -0600

    going to turn in, sadly couldnt get it to work

--- hood.js
+++ hood.js
@@ -2,21 +2,24 @@ const eatingBtn = document.querySelector('#eating')
 
 
 const randomPlace = () {
-    const num = Math.floor(Math.random() * 4)
-    
-    switch (num) {
+const num = Math.floor(Math.random()*4)
+    switch(num){
         case 0:
-            window.open('https://www.cafesicilia.com/', '_blank');
+         return  window.open('https://www.cafesicilia.com/', '_blank');
+            
         case 1:
-            window.open('https://www.raisingcanes.com', "_blank");
+          return  window.open('https://www.raisingcanes.com', "_blank");
+          
         case 2:
-            window.open('https://www.olivegarden.com/home', "_blank");
+           return window.open('https://www.olivegarden.com/home', "_blank");
+            
         case 3:
-            window.open('https://cicis.com/', "_blank");
+          return  window.open('https://cicis.com/', "_blank");
+            
     }
     
 }
 
 
 
-eatingBtn.addEventListener('click', randomPlace)
+eatingBtn.addEventListener('click', randomPlace())
